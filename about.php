f70ddff1720151a9c7ca5b0fa468f5bbfa504224
bungaviva123 <167213370+bungaviva123@users.noreply.github.com>
2024-04-17 04:57:44 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #b784a7"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Bunga Florist adalah toko bunga Indonesia terkemuka dengan 22 cabang (dapur pusat) yang mengelola lebih dari 400 outlet: Jabodetabek (Gajah Mada, Pondok Pinang, Jatinegara, Cikini, Sunter, Serpong, Ciputat, Pekayon, Bogor dan Karawang), Bandung, Surabaya, Lampung, Batam, Pekanbaru, Makassar, Manado, Bali, Solo, Semarang, Balikpapan, dan Samarinda. Kami masih terus memperluas secara nasional ke kota-kota lain.</p>
+<p>
+Bunga Florist  adalah salah satu pelopor toko bunga pertama dalam bisnis florist di Indonesia. Didirikan pada tahun 2022.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
